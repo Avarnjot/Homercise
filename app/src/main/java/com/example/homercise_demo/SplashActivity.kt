1c2f534abfe61b233ba14b6320a11e178e47556b
Avarnjot <200536422@student.georgianc.on.ca>
2024-03-21 13:11:33 -0400

    Updated Project

--- app/src/main/java/com/example/homercise_demo/SplashActivity.kt
+++ app/src/main/java/com/example/homercise_demo/SplashActivity.kt
@@ -3,14 +3,23 @@ package com.example.homercise_demo
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import com.bumptech.glide.Glide
 import java.util.logging.Handler
 
 class SplashActivity : AppCompatActivity() {
 
-        private val SPLASH_DELAY: Long = 1500
+        private val SPLASH_DELAY: Long = 2000
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
             setContentView(R.layout.activity_splash)
+
+            val imageView: ImageView = findViewById(R.id.loadrunning)
+            Glide.with(this)
+                .asGif()
+                .load(R.drawable.animation)
+                .into(imageView)
+//            imageView.setImageResource(R.drawable.loading)
 
             android.os.Handler().postDelayed({
                 val mainIntent = Intent(this, Login::class.java)
